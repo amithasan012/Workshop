f02007af597dbf2ccc6497aee61700ae9ff0cd85
M.R.Amit Hasan <amithasanmr@gmail.com>
2023-11-25 21:23:26 +0600

    New

---
+++ Webpage/webpage2.js
@@ -0,0 +1,15 @@
+var myimage=document.getElementById("image2")
+var mybutton1=document.getElementById("button1")
+var mybutton2=document.getElementById("button2")
+var mybutton3=document.getElementById("button3")
+
+mybutton1.addEventListener("click",function(){
+    myimage.src="24.jpeg"
+})
+mybutton2.addEventListener("click",function(){
+    myimage.src="23.jpeg"
+})
+mybutton3.addEventListener("click",function(){
+    myimage.src="22.jpeg"
+})
+
